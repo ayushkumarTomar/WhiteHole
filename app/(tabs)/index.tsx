00443d2226fdcc4a32b22fa8188d82a9cde1dd35
ayushKumarTomar <tomar2004ayush@gmail.com>
2025-02-20 02:30:00 +0530

    minor

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -40,9 +40,6 @@ const Home = () => {
   const [username , setUsername] = useState<string>("Tangerine");
   const [modalVisible, setModalVisible] = useState(false);
   const [quality, setQuality] = useState<'96' | '160' | '320'>('320');
-
-
-  const [tempName , setTempName] = useState<string>(username);
   useEffect(() => {
     const fetchLastSession = async () => {
       setContinueListeningData(await getLastSession());
@@ -66,9 +63,8 @@ const Home = () => {
   }
 
   const handleConfirmNameChange = () => {
-    setUsername(tempName);
     setModalVisible(false);
-    saveUsername(tempName);
+    saveUsername(username);
     if(quality!='320') setQuality(quality);   
   };
 
@@ -121,8 +117,8 @@ const Home = () => {
                 style={styles.input}
                 placeholder="Enter your name"
                 placeholderTextColor="#888"
-                value={tempName}
-                onChangeText={setTempName}
+                value={username}
+                onChangeText={setUsername}
               />
 
               <Text style={styles.modalSubtitle}>Select Quality</Text>
@@ -153,7 +149,6 @@ const Home = () => {
                 </View>
               )}
 
-              {/* Buttons */}
               <View style={styles.buttonRow}>
                 <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.cancelButton}>
                   <Text style={styles.buttonText}>Cancel</Text>
@@ -169,254 +164,6 @@ const Home = () => {
     </SafeAreaProvider>
   );
 };
-//   return (
-//     <SafeAreaProvider>
-//       <SafeAreaView style={styles.container}>
-//         <LinearGradient
-//           colors={[
-//             'rgba(14, 14, 14, 0.00)', 
-//             'rgba(16, 43, 45, 0.94)', 
-//             'rgba(6, 160, 181, 0)',  
-//           ]}
-//           style={styles.gradient}
-//           start={{ x: 0, y: 0 }}
-//           end={{ x: 0, y: 1 }}   
-//         />
-
-//         <ScrollView>
-//           <View style={styles.header}>
-//             <SimpleLineIcons name="music-tone-alt" size={24} color="white" />
-//             <Text style={styles.headerText}>
-//               Welcome back!{"\n"}
-//               {/* <Text style={[styles.headerText, { fontSize: vs(20) }]}>{username}</Text> */}
-//               <Text
-//               style={[styles.headerText, { fontSize: vs(20) }]}
-//               onPress={() => setModalVisible(true)}
-//             >
-//               {username}
-//             </Text>
-//             </Text>
-             
-//             <View style={styles.headerIcons}>
-//               <Feather name="bar-chart-2" size={18} color="white" />
-//               <Feather name="bell" size={18} color="white" />
-//               <Ionicons name="settings-outline" size={18} color="white" />
-//             </View>
-//           </View>
-
-//           <ContinueChart continueListeningData={continueListeningData} />
-//           <TrendingChart continueListeningData={new_trending || []} title="Trending" type="new_trending" />
-//           <TrendingChart continueListeningData={top_playlists || []} title="Handpicked Playlists" type="top_playlists" />
-//           <TrendingChart continueListeningData={new_albums || []} title="New Albums" type="new_albums" />
-//           {/* <TrendingChart continueListeningData={browse_discover || []} title="Discover" type="browse_discover" /> */}
-//           <TrendingChart continueListeningData={charts || []} title="Charts" type="charts" />
-//           {TopArtistData && <TopArtist data={TopArtistData} title='Top Artists'/>}
-//           {/* <TrendingChart continueListeningData={radio || []} title="Radio" type="radio" /> */}
-
-//           <View style={{ marginBottom: vs(50), flex: 1 }} />
-//         </ScrollView>
-//         <Modal
-//           animationType="slide"
-//           transparent={true}
-//           visible={modalVisible}
-//           onRequestClose={() => setModalVisible(false)}
-//         >
-//           <View style={styles.modalContainer}>
-//             <View style={styles.modalContent}>
-//               <Text style={styles.modalTitle}>Change Name</Text>
-//               <TextInput
-//                 style={styles.input}
-//                 placeholder="Enter your name"
-//                 placeholderTextColor="#888"
-//                 value={tempName}
-//                 onChangeText={setTempName}
-//               />
-
-//               {/* Quality Selector */}
-//               <Text style={styles.modalSubtitle}>Select Quality</Text>
-//               <TouchableOpacity
-//                 style={styles.dropdown}
-//                 onPress={() => setShowQualityPicker(true)}
-//               >
-//                 <Text style={styles.dropdownText}>{quality}</Text>
-//               </TouchableOpacity>
-
-//               {/* Quality Picker */}
-//               {showQualityPicker && (
-//                 <View style={styles.qualityPicker}>
-//                   {['Low', 'Medium', 'High'].map((option) => (
-//                     <TouchableOpacity
-//                       key={option}
-//                       style={[
-//                         styles.option,
-//                         quality === option && styles.selectedOption,
-//                       ]}
-//                       onPress={() => {
-//                         setQuality(option as 'Low' | 'Medium' | 'High');
-//                         setShowQualityPicker(false);
-//                       }}
-//                     >
-//                       <Text style={styles.optionText}>{option}</Text>
-//                     </TouchableOpacity>
-//                   ))}
-//                 </View>
-//               )}
-
-//               {/* Action Buttons */}
-//               <View style={styles.buttonRow}>
-//                 <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.cancelButton}>
-//                   <Text style={styles.buttonText}>Cancel</Text>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={handleConfirmNameChange} style={styles.confirmButton}>
-//                   <Text style={styles.buttonText}>Confirm</Text>
-//                 </TouchableOpacity>
-//               </View>
-//             </View>
-//           </View>
-//         </Modal>
-//         {/* <Modal
-//           animationType="slide"
-//           transparent={true}
-//           visible={modalVisible}
-//           onRequestClose={() => setModalVisible(false)}
-//         >
-//           <View style={styles.modalContainer}>
-//             <View style={styles.modalContent}>
-//               <Text style={styles.modalTitle}>Change Name</Text>
-//               <TextInput
-//                 style={styles.input}
-//                 placeholder="Enter your name"
-//                 placeholderTextColor="#888"
-//                 value={tempName}
-//                 onChangeText={setTempName}
-//               />
-//               <View style={styles.buttonRow}>
-//                 <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.cancelButton}>
-//                   <Text style={styles.buttonText}>Cancel</Text>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={handleConfirmNameChange} style={styles.confirmButton}>
-//                   <Text style={styles.buttonText}>Confirm</Text>
-//                 </TouchableOpacity>
-//               </View>
-//             </View>
-//           </View>
-//         </Modal> */}
-//          {/* <Modal
-//           animationType="slide"
-//           transparent={true}
-//           visible={modalVisible}
-//           onRequestClose={() => setModalVisible(false)}
-//         >
-//           <View style={styles.modalContainer}>
-//             <View style={styles.modalContent}>
-//               <Text style={styles.modalTitle}>Change Name</Text>
-//               <TextInput
-//                 style={styles.input}
-//                 placeholder="Enter your name"
-//                 placeholderTextColor="#888"
-//                 value={tempName}
-//                 onChangeText={setTempName}
-//               />
-
-//               {/* Quality Picker */}
-//               <Text style={styles.modalSubtitle}>Select Quality</Text>
-//               <Picker
-//                 selectedValue={quality}
-//                 onValueChange={(itemValue) => setQuality(itemValue)}
-//                 style={styles.picker}
-//               >
-//                 <Picker.Item label="Low" value="96" />
-//                 <Picker.Item label="Medium" value="160" />
-//                 <Picker.Item label="High" value="320" />
-//               </Picker>
-
-//               <View style={styles.buttonRow}>
-//                 <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.cancelButton}>
-//                   <Text style={styles.buttonText}>Cancel</Text>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={handleConfirmNameChange} style={styles.confirmButton}>
-//                   <Text style={styles.buttonText}>Confirm</Text>
-//                 </TouchableOpacity>
-//               </View>
-//             </View>
-//           </View>
-//         </Modal> */}
-//       </SafeAreaView>
-//     </SafeAreaProvider>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#1B1B1B',
-//   },
-//   gradient: {
-//     position: 'absolute',
-//     left: 0,
-//     right: 0,
-//     top: 0,
-//     height: "30%",
-//   },
-//   header: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     margin: vs(10),
-//   },
-//   headerText: {
-//     color: "white",
-//     fontFamily: "Montserrat_700Bold",
-//     marginLeft: s(10),
-//   },
-//   headerIcons: {
-//     position: 'absolute',
-//     right: 0,
-//     marginRight: s(5),
-//     gap: s(15),
-//     flexDirection: 'row',
-//   },
-//   modalContainer: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: 'rgba(0,0,0,0.6)',
-//   },
-//   modalContent: {
-//     backgroundColor: '#222',
-//     padding: vs(20),
-//     borderRadius: vs(10),
-//     width: '80%',
-//   },
-//   modalTitle: {
-//     color: '#fff',
-//     fontSize: vs(16),
-//     fontFamily: 'Montserrat_700Bold',
-//     marginBottom: vs(10),
-//   },
-//   input: {
-//     backgroundColor: '#333',
-//     color: '#fff',
-//     padding: vs(10),
-//     borderRadius: vs(5),
-//     marginBottom: vs(10),
-//   },
-//   buttonRow: {
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//   },
-//   cancelButton: {
-//     padding: vs(10),
-//   },
-//   confirmButton: {
-//     backgroundColor: '#0A84FF',
-//     padding: vs(10),
-//     borderRadius: vs(5),
-//   },
-//   buttonText: {
-//     color: '#fff',
-//     fontSize: vs(12),
-//   },
-// });
 
 const styles = StyleSheet.create({
   container: {
